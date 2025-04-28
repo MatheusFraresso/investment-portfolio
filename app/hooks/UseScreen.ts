db88c4023d4f7f80ce26718c75eb080b648f7596
MatheusFraresso <matheus@laptop-matheus.>
2025-04-28 08:19:05 -0300

    Added updated files

---
+++ app/hooks/UseScreen.ts
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+interface IScreenProperties {
+  isMobile: boolean;
+  width: number;
+  height: number;
+}
+
+function getScreenProperties() {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    isMobile: window.innerWidth <= MOBILE_BREAKPOINT,
+  };
+}
+export default function useScreen() {
+  const [screenProperties, setScreenProperties] = useState<IScreenProperties>(
+    getScreenProperties()
+  );
+
+  useEffect(() => {
+    const onResize = getScreenProperties;
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  return screenProperties;
+}
